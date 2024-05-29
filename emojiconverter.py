69579bded05a45ff6d076224e5874454d907a291
qinisonhlakaniphoxulu <qiniso.nhlakanipho.xulu@gmail.com>
2024-05-29 10:26:19 +0200

    first commit

---
+++ emojiconverter.py
@@ -0,0 +1,11 @@
+#Emoji Converter
+message = input(">")
+words = message.split(' ')
+emojis = {
+    ":)" : "😀",
+    ":(" : "😔"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
